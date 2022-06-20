c8cac25d0c1be55162698c71ea304ff7c319ca1b
zhangboyu <zhangboyu@hosecloud.com>
2022-06-20 17:14:12 +0800

    fix：type

--- src/array.ts
+++ src/array.ts
@@ -1,9 +1,9 @@
 import { getType } from "./base"
 import { IArrayRandom, IArrayUniq, IArrayDemote, IDemoteArray } from "./types"
 
-export const arrayRandom: IArrayRandom<any[]> = (arr) => arr.sort(() => Math.random() - 0.5);
+export const arrayRandom: IArrayRandom<any[]> = arr => arr.sort(() => Math.random() - 0.5);
 
-export const arrayUniq: IArrayUniq<any[]> = (arr: any[]) => Array.from(new Set(arr))
+export const arrayUniq: IArrayUniq<any[]> = arr => Array.from(new Set(arr))
 
 export const arrayDemote: IArrayDemote<IDemoteArray<any>> = (arr, result = []) => {
     arr.forEach(i => getType(i) === "array" ? arrayDemote(i, result) : result.push(i))
